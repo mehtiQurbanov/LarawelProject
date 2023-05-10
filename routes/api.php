9ba5e439d6ccb69a90126cddb8fe0a90e284e37c
Lenovo <metiy2032@gmail.com>
2023-05-10 22:36:08 +0300

    project updated

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
+
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+Route:: get('/messege',function(){
+    return response()->jsone([
+'messege'=>'hello'
+    ]);
 });
--- routes/web.php
+++ routes/web.php
@@ -23,9 +23,12 @@ Route::get('/', function () {
 Route::get ('/mehdi','App\Http\Controllers\mehdicontroller@index');
 Route::get ('/mehdi/{$ad}','App\Http\Controllers\mehdicontroller@person');
 Route::get('/connect','App\Http\controllers\connectcontroller@connect');
+Rout :: get('/connect','App\Http\controllers\Haqqindacontroller@index');
+
+
 
 //  university tasks//
-Route:: get ('/owners',[ownerController::class, 'index'])->name('owners.index')->middleware(['replace']);
+Route:: get ('/owners',[OwnerController::class, 'index'])->name('owners.index')->middleware(['replace']);
 Route::get('/cars', [CarController::class, 'index'])->name('cars.index')->middleware(['replace']);
 
 Route::middleware(['auth'])->group(function () {
@@ -37,3 +40,7 @@ Route::middleware(['auth'])->group(function () {
     Route::resource("owners", OwnerController::class)->except(['index'])->middleware('admin');
     Route::resource("cars", CarController::class)->except(['index'])->middleware('admin');
 });
+
+Route::get('/setLanguage/{lang}', [LanguageController::class, 'setLanguage'])->name("lang");
+Route ::apiResource('name',UserController::class)->only(['show','store','update','destroy']);
+
